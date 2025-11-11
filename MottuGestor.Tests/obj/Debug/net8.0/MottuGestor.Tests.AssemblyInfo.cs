da0022ab6fccbc35be45d5a4e778b2665cb9427d
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2025-11-11 18:10:33 -0300

    first commit

--- MottuGestor.Tests/obj/Debug/net8.0/MottuGestor.Tests.AssemblyInfo.cs
+++ MottuGestor.Tests/obj/Debug/net8.0/MottuGestor.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MottuGestor.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f59a0ff042eb13fd0e2af5c512300973140a8f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db4e89f0b883083acc127ec11285d712c9d073b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MottuGestor.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MottuGestor.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
